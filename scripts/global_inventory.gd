a8084f09b5b88269680315485fb04b364cde4564
Blake Arnold <barnold@blakearnold.me>
2025-08-17 07:54:32 -0400

    WIP - Inventory system items

--- scripts/global_inventory.gd
+++ scripts/global_inventory.gd
@@ -1,4 +1,3 @@
-# TODO: Items should be received by the server instead of instantiated locally
 extends Node
 
 signal bags_updated
@@ -6,19 +5,22 @@ signal bags_updated
 var bags: Array[Item_Template]:
 	get: return bags
 	# TODO: We will eventually like to do some validation on the bags array
-	set(value): 
+	set(value):
+		if value.size() > 4:
+			return
 		bags = value
 		bags_updated.emit()
 
 func _ready() -> void:
+	# TODO: Items should be received by the server instead of instantiated locally
 	var new_bag: Array[Item_Template] = bags.duplicate()
 	new_bag.append(Item_Template.new(
-		1, 
-		Item_Template.ITEM_CLASS.CONTAINER, 
-		Item_Template.ITEM_SUBCLASS.BAG, 
-		"Backpack", 
-		preload("res://prefabs/backpack.tres"), 
-		false, 
+		1,
+		Item_Template.ITEM_CLASS.CONTAINER,
+		Item_Template.ITEM_SUBCLASS.BAG,
+		"Backpack",
+		preload("res://prefabs/backpack.tres"),
+		false,
 		8,
 		"A sturdy backpack for carrying items."
 	))
